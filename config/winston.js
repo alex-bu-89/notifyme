a340de587ca9c464794e6ab42c04f94ad4d8b91c
Alexander Buyanov <alex.bu.89@gmail.com>
2017-12-17 15:09:19 +0100

    clean up; remove api; add linter; add logger

---
+++ config/winston.js
@@ -0,0 +1,16 @@
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, printf, colorize, simple } = format;
+
+const myFormat = printf(info => {
+  return `${info.timestamp} ${info.level}: ${info.message}`;
+});
+
+module.exports = createLogger({
+  transports: [new transports.Console()],
+  format: combine(
+    colorize({ all: true }),
+    simple(),
+    timestamp(),
+    myFormat
+  ),
+});
--- src/lib/checkAppointment.js
+++ src/lib/checkAppointment.js
@@ -1,54 +1,56 @@
-var q = require('q');
-var webdriverio = require('webdriverio');
-var options = { desiredCapabilities: { browserName: 'chrome' } };
-var client = webdriverio.remote(options);
-var testConf = require('./../config.json').testConf;
-var cheerio = require('cheerio');
-var parseMonthString = require('./util.js').parseMonthString;
+const webdriverio = require('webdriverio');
+const config = require('config');
 
-const MONTHS_TO_CHECK = 2
-let availableAppointments = []
+const options = { desiredCapabilities: { browserName: 'chrome' } };
+const client = webdriverio.remote(options);
+const logger = require('../../config/winston');
+// const { parseMonthString } = require('./util.js');
+//
+// const MONTHS_TO_CHECK = 2;
+// const availableAppointments = [];
 
-let setFreeAppoitments = (html) => {
-  var $ = cheerio.load(html),
-      cells = $('.CELL'),
-      month = parseMonthString($('#month').find('span').text()),
-      year = $('#year').find('span').text();
-
-  cells.map(function(){
-    console.log($(this).find('div > span').text());
-    if($(this).find('div > span').text() === '1'){ // 1 in span == available appointment
-
-      var day = $(this).find('a > span').text();
-      var date = day + '-' + month + '-' + year;
-      console.log(date);
-      availableAppointments.push(date)
-    }
-  });
-}
-
-client
-  .init()
-  .url(testConf.startUrl)
-  .click('#btnTerminBuchen')
-  .selectByValue('#cobStaat', 160) // Staatsangehörigkeit
-  .selectByValue('#cobFamAngInBerlin', 'Nein') // Familienangehörigen in EU Ja / Nein
-  .selectByValue('#cobAnliegen', 305304) // Anliegen *
-  .click('#cbZurKenntnis')
-  .click('#labNextpage')
-  .setValue('#tfFirstName', 'Test') // Vorname
-  .setValue('#tfLastName', 'Test') // Nachname
-  .selectByValue('#cobGebDatumTag', 1) // Geb. Datum
-  .selectByValue('#cobGebDatumMonat', 1)
-  .setValue('#tfGebDatumJahr', 2000)
-  .selectByValue('#cobVPers', 1) // Anzahl der Personen
-  .setValue('#tfMail', 'test@gmail.com') // Email
-  .selectByValue('#cobGenehmigungBereitsVorhanden', 'Ja') // Besitzen Sie bereits eine Aufenthaltserlaubnis ?
-  .setValue('#tfEtNr', '12345') // Nummer der Aufenthaltserlaubnis
-  .click('#txtNextpage') // double click on next button...bug?
-  .click('#txtNextpage')
-  .getHTML('body')
-    .then((html) => {
-
-    })
-  .click('#labnextMonth')
+// let setFreeAppoitments = (html) => {
+//   const $ = cheerio.load(html),
+//     cells = $('.CELL'),
+//     month = parseMonthString($('#month').find('span').text()),
+//     year = $('#year').find('span').text();
+//
+//   cells.map(function() {
+//     console.log($(this).find('div > span').text());
+//     if ($(this).find('div > span').text() === '1') {
+//
+//       const day = $(this).find('a > span').text();
+//       const date = day + '-' + month + '-' + year;
+//       console.log(date);
+//       availableAppointments.push(date)
+//     }
+//   });
+// }
+logger.info('Start webdriverio');
+logger.info(logger);
+// client
+//   .init()
+//   .url(config.get('berlinde.url'))
+//   .click('#btnTerminBuchen')
+//   .selectByValue('#cobStaat', 160) // Staatsangehörigkeit
+//   .selectByValue('#cobFamAngInBerlin', 'Nein') // Familienangehörigen in EU Ja / Nein
+//   .selectByValue('#cobAnliegen', 305304) // Anliegen *
+//   .click('#cbZurKenntnis')
+//   .click('#labNextpage')
+//   .setValue('#tfFirstName', 'Test') // Vorname
+//   .setValue('#tfLastName', 'Test') // Nachname
+//   .selectByValue('#cobGebDatumTag', 1) // Geb. Datum
+//   .selectByValue('#cobGebDatumMonat', 1)
+//   .setValue('#tfGebDatumJahr', 2000)
+//   .selectByValue('#cobVPers', 1) // Anzahl der Personen
+//   .setValue('#tfMail', 'test@gmail.com') // Email
+//   .selectByValue('#cobGenehmigungBereitsVorhanden', 'Ja') // Besitzen Sie bereits eine Aufenthaltserlaubnis ?
+//   .setValue('#tfEtNr', '12345') // Nummer der Aufenthaltserlaubnis
+//   .click('#txtNextpage') // double click on next button...bug?
+//   .click('#txtNextpage')
+//   .getHTML('body')
+//   .then((html) => {
+//     logger.info('Html result');
+//     logger.debug(html);
+//   })
+//   .click('#labnextMonth');
