1ef680c6800d979fee70166a81d0a7dc7abfa45b
alex-bu-89 <alex.bu.89@gmail.com>
2018-04-20 16:58:01 +0200

    add phantom

--- termin-svc/src/controllers/appointment.js
+++ termin-svc/src/controllers/appointment.js
@@ -1,8 +1,20 @@
 const logger = require('../../config/logger');
+const phantom = require('phantom');
 
 module.exports.get = async (ctx) => {
+  const instance = await phantom.create();
+  const page = await instance.createPage();
+  await page.on('onResourceRequested', (requestData) => {
+    console.info('Requesting', requestData.url);
+  });
+
+  const status = await page.open('https://stackoverflow.com/');
+  const content = await page.property('content');
+
+  await instance.exit();
+
   ctx.response.body = {
     status: 200,
-    data: { hello: 'world' },
+    data: JSON.stringify(content),
   };
 };
