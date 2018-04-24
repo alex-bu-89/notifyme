47129822288a4e03ad22e7b3a87c97e974e5de2c
alex-bu-89 <alex.bu.89@gmail.com>
2018-04-24 13:30:19 +0200

    minor fixes

--- termin-svc/src/controllers/appointment.js
+++ termin-svc/src/controllers/appointment.js
@@ -1,27 +1,7 @@
 const config = require('config');
-const phantom = require('phantom');
 const logger = require('../../config/logger');
 
 module.exports.get = async (ctx) => {
-  const instance = await phantom.create();
-  const page = await instance.createPage();
-  await page.on('onResourceRequested', (requestData) => {
-    console.info('Requesting', requestData.url);
-  });
-
-  const status = await page.open(config.get('startUrl'));
-  // const button = await page.evaluate(function() {
-  //   return document.getElementById('foo').innerHTML;
-  // }).then(function(html){
-  //   console.log(html);
-  // });
-
-  // const content = await page.property('content');
-
-  // console.log('------>', content);
-
-  await instance.exit();
-
   ctx.response.body = {
     status: 200,
     data: 'working',
