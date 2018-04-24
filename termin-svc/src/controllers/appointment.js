4d8fbb5da990f6f7ff11570cc22ba938c11da0f6
alex-bu-89 <alex.bu.89@gmail.com>
2018-04-24 14:00:51 +0200

    test silenium

--- termin-svc/src/controllers/appointment.js
+++ termin-svc/src/controllers/appointment.js
@@ -1,9 +1,28 @@
 const config = require('config');
+const webdriverio = require('webdriverio');
 const logger = require('../../config/logger');
 
+const options = { desiredCapabilities: { browserName: 'chrome' } };
+// const options = { desiredCapabilities: { browserName: 'phantomjs' } };
+const client = webdriverio.remote(options);
+
 module.exports.get = async (ctx) => {
-  ctx.response.body = {
-    status: 200,
-    data: 'working',
-  };
+  logger.info('Start webdriverio');
+
+  client
+    .init()
+    .url(config.get('startUrl'))
+    .getHTML('body')
+    .then((html) => {
+      logger.info(html);
+
+      ctx.response.body = {
+        status: 200,
+        data: 'working',
+      };
+    })
+    .catch((error) => {
+      logger.error(error);
+      throw error;
+    });
 };
