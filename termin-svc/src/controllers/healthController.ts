96eed9acc0ed41355f60fdcc4ac8d1b01cd9bc3c
Alexander Buyanov <buyanov15@gmail.com>
2020-12-04 16:33:59 +0100

    Rename project

--- termin-svc/src/controllers/healthController.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-
-/**
- * GET /
- * Healthcheck route
- */
-export function index(req: Request, res: Response) {
-	res.json({ status: 'OK' });
-}
--- termin-svc/src/router.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import terminController from './controllers/terminController';
-
-const router = express.Router();
-
-router.get('/termins', terminController.getAll);
-
-export default router;
