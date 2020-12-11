8b29256825a07a9e662c17d2d671cc0ed020077e
Alexander Buyanov <buyanov15@gmail.com>
2020-12-11 11:33:32 +0100

    Use cron job

--- src/handler.ts
+++ src/handler.ts
@@ -24,8 +24,8 @@ export async function notify() {
       await Notifier.register([Notifier.clients.TELEGRAM], msg);
     }));
   } else {
-    // const msg = `<b>No products available</b>\n${new Date().toString()}\n<pre>${JSON.stringify(result)}</pre>`;
-    // await Notifier.register([Notifier.clients.TELEGRAM], msg, true);
+    const msg = `<b>No products available</b>\n${new Date().toString()}\n<pre>${JSON.stringify(result)}</pre>`;
+    await Notifier.register([Notifier.clients.TELEGRAM], msg, true);
   }
 
   logger.info(`Result: ${JSON.stringify(result)}`);
